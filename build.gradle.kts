2ade450f7d25fdd7ce025871ac13e5fbce450b00
Philosobyte <philosobyte@gmail.com>
2024-07-10 12:35:30 -0400

    add h2 dependency and switch to Java 17 for compatibility with Ignite 2.14

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
@@ -25,7 +25,6 @@ configurations {
 repositories {
     mavenCentral()
 }
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter:3.3.1")
     implementation("org.springframework.boot:spring-boot-starter-log4j2:3.3.1")
@@ -38,6 +37,8 @@ dependencies {
             classifier = "tests"
         }
     }
+    // this has CVEs, but it is the version used by Ignite 2.16
+    testImplementation("com.h2database:h2:1.4.197")
     testImplementation("org.junit.vintage:junit-vintage-engine:5.10.3")
 
     compileOnly("org.projectlombok:lombok")
@@ -47,62 +48,6 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
 }
-
-//application {
-//    applicationDefaultJvmArgs = listOf(
-//        "--add-opens", "java.base/jdk.internal.access=ALL-UNNAMED",
-//        "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
-//        "--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED",
-//        "--add-opens", "java.base/sun.util.calendar=ALL-UNNAMED",
-//        "--add-opens", "java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED",
-//        "--add-opens", "jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED",
-//        "--add-opens", "java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED",
-//        "--add-opens", "jdk.management/com.sun.management.internal=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.io=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.nio=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.net=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.util=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.util.concurrent=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.util.concurrent.locks=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.util.concurrent.atomic=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.lang.invoke=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.math=ALL-UNNAMED",
-//        "--add-opens", "java.sql/java.sql=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.time=ALL-UNNAMED",
-//        "--add-opens", "java.base/java.text=ALL-UNNAMED",
-//        "--add-opens", "java.management/sun.management=ALL-UNNAMED",
-//        "--add-opens", "java.desktop/java.awt.font=ALL-UNNAMED"
-//    )
-//}
-//
-//tasks.withType<JavaExec> {
-//    jvmArgs("--add-opens", "java.base/jdk.internal.access=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/sun.util.calendar=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "jdk.management/com.sun.management.internal=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.io=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.nio=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.net=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.util=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.util.concurrent=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.util.concurrent.locks=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.util.concurrent.atomic=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.lang.invoke=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.math=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.sql/java.sql=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.lang.reflect=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.base/java.text=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.management/sun.management=ALL-UNNAMED")
-//    jvmArgs("--add-opens", "java.desktop/java.awt.font=ALL-UNNAMED")
-//}
 
 tasks.withType<Test> {
     useJUnitPlatform {
